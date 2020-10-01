77632c816e508c85b9e3839a6d1c56c6cab064b9
ujjwalskyhunter <72217976+ujjwalskyhunter@users.noreply.github.com>
2020-10-01 22:30:05 +0530

    Create testworld.js

---
+++ testworld.js
@@ -0,0 +1,24 @@
+var gifTimer;
+var currentGifId=null;
+var step = 100; //height of a thumbnail
+$('.thumbnail-wrapper img').hover(
+   function(){
+      currentGifId = $(this)
+      gifTimer = setInterval(playGif,500);
+   },
+   function(){
+       clearInterval(gifTimer);
+       currentGifId=null;
+   }
+);
+
+var playGif = function(){
+   var top = parseInt(currentGifId.css('top'))-step;
+   var max = currentGifId.height();
+   console.log(top,max)
+   if(max+top<=0){
+     console.log('reset')
+     top=0;
+   }
+   currentGifId.css('top',top);
+}
